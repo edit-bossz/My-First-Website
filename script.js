5b3734f1d4dcdd6b0be3a10a40a0b5f8cd683189
edit-bossz <manjarimondal06@gmail.com>
2024-11-14 15:53:18 +0530

    Auto commit

--- script.js
+++ script.js
@@ -19,26 +19,6 @@ function hideButton() {
 // Add event listeners to the image section
 imageSection.addEventListener('mouseenter', showButton);
 imageSection.addEventListener('mouseleave', hideButton);
-
-// Function to detect if the user is on an Android mobile device
-function isAndroidDevice() {
-    return /Android/i.test(navigator.userAgent);
-}
-
-// Function to update the contact link based on device type
-function updateContactLink() {
-    const contactLink = document.getElementById('contact-link');
-    
-    if (contactLink) {
-        if (isAndroidDevice()) {
-            // Update href to open Gmail app on Android
-            contactLink.href = 'intent://send?to=editprime7@gmail.com&subject=Subject&body=Body#Intent;scheme=mailto;package=com.google.android.gm;end';
-        } else {
-            // Keep the link for opening Gmail in a web browser on other devices
-            contactLink.href = 'https://mail.google.com/mail/?view=cm&fs=1&to=editprime7@gmail.com';
-        }
-    }
-}
-
-// Call the function to update the link when the page loads
-document.addEventListener("DOMContentLoaded", updateContactLink);
+// Touch events for mobile
+imageSection.addEventListener('touchstart', showButton);
+imageSection.addEventListener('touchend', hideButton);
